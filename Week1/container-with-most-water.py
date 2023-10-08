e3bc789b41b05b85a3a0c07237245263596f5035
MeetOswal <71141902+MeetOswal@users.noreply.github.com>
2023-10-07 22:18:25 -0400

    Update container-with-most-water.py

--- Week1/container-with-most-water.py
+++ Week1/container-with-most-water.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_water = 0
+        i = 0
+        j = len(height) - 1
+        while(i < j):
+            area = min(height[i], height[j]) * abs(j - i)
+            if(area > max_water):
+                max_water = area
+            
+            if(height[i] < height[j]):
+                i += 1
+            else:
+                j -= 1
+        return max_water
